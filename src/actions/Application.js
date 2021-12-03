f735e7f6331aa91b8f72b3a75c232b2b47c02e7d
Anh_Le <lehoangmaianh126@gmail.com>
2021-12-03 16:18:41 -0500

    Finished all file 1,2,3 for /adoption

--- src/actions/Application.js
+++ src/actions/Application.js
@@ -8,5 +8,76 @@ import fetch from "isomorphic-unfetch"
 import urls from "../../utils/urls"
 
 export const submitApplication = (application) => {
-    
+    fetch(urls.dbUrl + urls.adoption.newApplication, {
+        method: "POST",
+        mode: "same-origin",
+        credentials: "include",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify( {application} )
+    })
+    .then((response) => response.json())
+    .then((json) => {
+        if (json == null) {
+            throw new Error("Could not connect to API")
+        } else if (!json.success) {
+            throw new Error(json.message)
+        }
+        return json.payload
+    })
+};
+
+export const getAllApplications = (catID) => {
+    fetch(urls.dbUrl + urls.adoption.applicationQuery + `?catID=${catID}`, {
+        method: "GET",
+        mode: "same-origin",
+        credentials: "include"
+    })
+    .then((response) => response.json())
+    .then((json) => {
+        if (json == null) {
+            throw new Error("Could not connect to API.")
+        } else if (!json.success) {
+            throw new Error(json.message)
+        } 
+        return json.payload
+    })
+}
+
+export const getApplicationInfo = (applicationID) => {
+    fetch(urls.dbUrl + urls.adoption.applicationQuery + `?applicationID=${applicationID}`, {
+        method: "GET",
+        mode: "same-origin",
+        credentials: "include"
+    })
+    .then((response) => response.json())
+    .then((json) => {
+        if (json == null) {
+            throw new Error("Could not connect to API.")
+        } else if (!json.success) {
+            throw new Error(json.message)
+        } 
+        return json.payload
+    })
+}
+
+export const setApproved = (applicationID) => {
+    fetch(urls.dbUrl + urls.adoption.applicationQuery + `?applicationID=${applicationID}`, {
+        method: "PUT",
+        mode: "same-origin",
+        credentials: "include",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    .then((response) => response.json())
+    .then((json) => {
+        if (json == null) {
+            throw new Error("Could not connect to API.")
+        } else if (!json.success) {
+            throw new Error(json.message)
+        } 
+        return json.success
+    })
 }
